febd67dd916d279dd6bfcae508adcad2f1e25793
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-07-23 21:38:36 +0200

    angular starting tutorial

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,9 @@
 greet('Sven'); //Zahlen (number) gehen nicht nur Strings (wörter)
-
+/**
+ * 
+ * @param name - this is the name at string
+ * @returns hello + name
+ */
 async function greet(name: string){
     return 'Hello ' + name;
 }
